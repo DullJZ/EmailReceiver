2293bbcc1047ecccbe87152f72390d69abeea54c
DullJZ <79080562+DullJZ@users.noreply.github.com>
2022-07-21 01:00:16 +0800

    添加ID记忆功能，优化检查非法文件名

---
+++ README.md
@@ -0,0 +1,2 @@
+# EmailReceive
+使用 EmailReceiver 接收邮件并用收件人加以区分
--- main.py
+++ main.py
@@ -17,16 +17,23 @@ def get_account_info():
     return account, password, pop3_server, ssl, port
 
 
-def save_mails(mails):
+def save_mails(mail):
     pattern = re.compile(r'<.*?>')
-    for mail in mails:
-        name = str(mail.get('date')).replace(':', '.') + '_' + mail.get('subject').replace(':', '.') + '.eml'
+    name = validate_name(str(mail.get('date'))) + '_' + validate_name(mail.get('subject')) + '.eml'
+    if pattern.search(mail.get('to')):
         receiver = pattern.search(mail.get('to')).group()[1:-1]
-        if not os.path.exists('./emails/'+receiver):
-            os.makedirs('./emails/'+receiver)
-        zmail.save(mail, target_path='./emails/' + receiver,
-                   name=name, overwrite=True)
+    else:
+        receiver = mail.get('to')
+    if not os.path.exists('./emails/' + receiver):
+        os.makedirs('./emails/' + receiver)
+    zmail.save(mail, target_path='./emails/' + receiver,
+               name=name, overwrite=True)
 
+
+def validate_name(name):  # 验证文件名是否合法
+    rstr = r"[\/\\\:\*\?\"\<\>\|]"  # '/ \ : * ? " < > |'
+    new_name = re.sub(rstr, ".", name)  # 替换为.
+    return new_name
 
 # 登录POP3服务器
 account, password, pop3_server, ssl, port = get_account_info()
@@ -35,15 +42,34 @@ server = zmail.server(account, password, pop_host=pop3_server, pop_ssl=ssl, pop_
 # 检测是否登录成功
 if server.pop_able():
     print('POP3 登录成功')
+
+    # 判断已检索的ID
+    if os.path.exists('./id_now.txt'):
+        with open('./id_now.txt', 'r') as f:
+            id_now = int(f.readline())
+    else:
+        id_now = 1
+        with open('./id_now.txt', 'w') as f:
+            f.write(str(id_now))
+    # 最新ID
     id_latest = server.stat()[0]
+
 else:
     raise Exception('POP3 配置错误')
 
-# 循环监听新邮件
+# 循环监听邮件
 while True:
-    mail = server.get_latest()
-    if mail is not None and mail.get('id') > id_latest:
-        print('收到一封邮件')
-        save_mails([mail])
-        id_latest = mail.get('id')
+    if id_now <= id_latest:
+        mail = server.get_mail(id_now)
+
+        if mail is not None:
+            print('收到一封邮件')
+            save_mails(mail)
+            # 保存当前ID
+            with open('./id_now.txt', 'w') as f:
+                f.write(str(id_now))
+            id_now += 1
+
+    # 更新最新ID
+    id_latest = server.stat()[0]
     time.sleep(1)
